060cbc480339d21c585470eaa47338292a3357c6
Кристина Ратник <kristi.2001@yandex.ru>
2022-08-31 15:26:56 +0300

    fix admin

--- core/admin.py
+++ core/admin.py
@@ -2,12 +2,68 @@ from django.contrib import admin
 
 # Register your models here.
 from core.models import User
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 @admin.register(User)
-class BoardAdmin(admin.ModelAdmin):
-    list_display = ("username", "email", "first_name", "last_name")
-    search_fields = ("email", "first_name", "last_name", "username")
-    list_filter = ("is_staff", "is_active", "is_superuser")
-    exclude = ("password",)
-    readonly_fields = ("last_login", "date_joined")
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    readonly_fields = ('last_login', 'date_joined')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (
+            'Personal info',
+            {
+                'fields': (
+                    'first_name',
+                    'last_name',
+                    'email',
+                )
+            },
+        ),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                )
+            },
+        ),
+        (
+            'Important dates',
+            {
+                'fields': (
+                    'last_login',
+                    'date_joined',
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (None, {'fields': ('username', 'password1', 'password2')}),
+        (
+            'Personal info',
+            {
+                'fields': (
+                    'first_name',
+                    'last_name',
+                    'email',
+                )
+            },
+        ),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                )
+            },
+        ),
+    )
