12cae720259237b4213a2f69d5464a36eb4c5b40
Кристина Ратник <kristi.2001@yandex.ru>
2022-09-30 15:45:00 +0300

    fix admin

--- goals/admin.py
+++ goals/admin.py
@@ -1,20 +1,36 @@
 from django.contrib import admin
 
-from goals.models import GoalCategory, Goal, GoalComment
+from goals.models import GoalCategory, Goal, GoalComment, Board, BoardParticipant
 
 
 class GoalCategoryAdmin(admin.ModelAdmin):
     list_display = ("title", "user", "created", "updated")
-    search_fields = ("title", "user")
+    search_fields = ("title", "user__username")
+
 
 class GoalAdmin(admin.ModelAdmin):
-    list_display = ("title", "description","status", "priority", "due_date", "category", "user", "created", "updated")
-    search_fields = ("title", "user", "category")
+    list_display = ("title", "description", "status", "priority", "due_date", "category", "user", "created", "updated")
+    search_fields = ("title", "user__username", "category__title")
+    list_filter = ("status", "priority", "due_date")
+
 
 class GoalCommentAdmin(admin.ModelAdmin):
     list_display = ("goal", "text", "user", "created", "updated")
-    search_fields = ("title", "user", "category")
+    search_fields = ("text", "user__username")
+
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("title", "created", "updated")
+    search_fields = ("title", )
+
+
+class BoardParticipantAdmin(admin.ModelAdmin):
+    list_display = ("board", "user", "role")
+    search_fields = ("board__title", "user__username")
+
 
 admin.site.register(GoalCategory, GoalCategoryAdmin)
 admin.site.register(Goal, GoalAdmin)
 admin.site.register(GoalComment, GoalCommentAdmin)
+admin.site.register(Board, BoardAdmin)
+admin.site.register(BoardParticipant, BoardParticipantAdmin)
